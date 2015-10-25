744a3d453d2328b112abee85058a8653f8818a72
韩冬 <handong05@meituan.com>
2015-10-25 09:54:50 +0800

    up

--- benchmark/madeup-parallel/action-callback.js
+++ benchmark/madeup-parallel/action-callback.js
@@ -3,7 +3,6 @@ var Action = require('action-js');
 require('../lib/fakesP');
 
 module.exports = function upload(stream, idOrPath, tag, done) {
-    var queries = new Array(global.parallelQueries);
     var tx = db.begin();
     var current = 0;
     var total = global.parallelQueries;
@@ -21,7 +20,7 @@ module.exports = function upload(stream, idOrPath, tag, done) {
             }
         }
     }
-    for( var i = 0, len = queries.length; i < len; ++i ) {
-        queries[i] = FileVersion.insert({index: i}).execWithin(tx)._go(callback);
+    for( var i = 0, len = total; i < len; ++i ) {
+        FileVersion.insert({index: i}).execWithin(tx)._go(callback);
     }
 }
